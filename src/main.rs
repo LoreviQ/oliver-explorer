152c7b480373b6b208efb203e858393a80c0acff
LoreviQ <lorevi@protonmail.com>
2025-03-05 20:33:41 +0000

    Added parse html function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
+use scraper::{Html, Selector};
 use std::error::Error;
 
 fn main() {
-    match fetch_url("https://www.oliver.tj/") {
-        Ok(html) => println!("Fetched HTML: {}", html),
-        Err(e) => println!("Error: {}", e),
-    }
+    let url = "https://www.oliver.tj/";
+    let html = fetch_url(url).unwrap();
+    let text_content = parse_html(&html).unwrap();
+    println!("Text content: {}", text_content);
 }
 
 fn fetch_url(url: &str) -> Result<String, Box<dyn Error>> {
     let body = reqwest::blocking::get(url)?.text()?;
     Ok(body)
+}
+
+fn parse_html(html: &str) -> Result<String, Box<dyn Error>> {
+    let document = Html::parse_document(&html);
+    let body_selector = Selector::parse("body").unwrap();
+    let text_content = document
+        .select(&body_selector)
+        .flat_map(|element| element.text())
+        .collect::<Vec<_>>()
+        .join(" ");
+
+    Ok(text_content)
 }
