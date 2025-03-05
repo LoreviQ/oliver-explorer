e57912957fcbfb51339a5ee6925b4f68e3e7d2c5
LoreviQ <lorevi@protonmail.com>
2025-03-05 20:30:44 +0000

    Added fetchurl function (currently targeting my website)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    match fetch_url("https://www.oliver.tj/") {
+        Ok(html) => println!("Fetched HTML: {}", html),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+fn fetch_url(url: &str) -> Result<String, Box<dyn Error>> {
+    let body = reqwest::blocking::get(url)?.text()?;
+    Ok(body)
 }
