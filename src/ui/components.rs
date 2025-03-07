8f997f043b002dcb320499dc8ffb47cb4f4f3405
LoreviQ <lorevi@protonmail.com>
2025-03-07 20:58:20 +0000

    extracted tabitem

--- src/ui/components.rs
+++ src/ui/components.rs
@@ -2,6 +2,28 @@ use crate::ui::config;
 use crate::ui::types;
 use eframe::egui;
 
+pub struct TabItem<'a> {
+    pub tab: &'a types::Tab,
+    pub is_active: bool,
+}
+
+impl<'a> TabItem<'a> {
+    pub fn new(tab: &'a types::Tab, is_active: bool) -> Self {
+        Self { tab, is_active }
+    }
+
+    pub fn show(&self, ui: &mut egui::Ui) -> bool {
+        let url = self.tab.get_url();
+        let tab_name = if url.len() > 20 {
+            format!("{}...", &url[..20])
+        } else {
+            url.clone()
+        };
+
+        ui.selectable_label(self.is_active, tab_name).clicked()
+    }
+}
+
 pub struct TabBar<'a> {
     pub active_tab: usize,
     pub tabs: &'a mut Vec<types::Tab>,
@@ -15,17 +37,8 @@ impl<'a> TabBar<'a> {
     pub fn show(&mut self, ui: &mut egui::Ui) -> usize {
         ui.horizontal(|ui| {
             for (index, tab) in self.tabs.iter().enumerate() {
-                let url = tab.get_url();
-                let tab_name = if url.len() > 20 {
-                    format!("{}...", &url[..20])
-                } else {
-                    url.clone()
-                };
-
-                if ui
-                    .selectable_label(self.active_tab == index, tab_name)
-                    .clicked()
-                {
+                let tab_item = TabItem::new(tab, self.active_tab == index);
+                if tab_item.show(ui) {
                     self.active_tab = index;
                 }
             }
