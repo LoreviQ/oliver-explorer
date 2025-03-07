68cf271e81a999aa24e1aba7e17dfcef7a42908d
LoreviQ <lorevi@protonmail.com>
2025-03-07 21:13:38 +0000

    Based tab height on config and refactored tabbar

--- src/ui/components.rs
+++ src/ui/components.rs
@@ -2,28 +2,6 @@ use crate::ui::config;
 use crate::ui::types;
 use eframe::egui;
 
-pub struct TabItem<'a> {
-    pub tab: &'a types::Tab,
-    pub is_active: bool,
-}
-
-impl<'a> TabItem<'a> {
-    pub fn new(tab: &'a types::Tab, is_active: bool) -> Self {
-        Self { tab, is_active }
-    }
-
-    pub fn show(&self, ui: &mut egui::Ui) -> bool {
-        let url = self.tab.get_url();
-        let tab_name = if url.len() > 20 {
-            format!("{}...", &url[..20])
-        } else {
-            url.clone()
-        };
-
-        ui.selectable_label(self.is_active, tab_name).clicked()
-    }
-}
-
 pub struct TabBar<'a> {
     pub active_tab: usize,
     pub tabs: &'a mut Vec<types::Tab>,
@@ -36,14 +14,20 @@ impl<'a> TabBar<'a> {
 
     pub fn show(&mut self, ui: &mut egui::Ui) -> usize {
         ui.horizontal(|ui| {
+            // Tab bar height
+            ui.set_min_height(config::TAB_BAR_HEIGHT);
+
+            // Tab items
             for (index, tab) in self.tabs.iter().enumerate() {
-                let tab_item = TabItem::new(tab, self.active_tab == index);
-                if tab_item.show(ui) {
+                let tab_item = TabItem::new(tab, self.active_tab == index).show(ui);
+                if tab_item.clicked() {
                     self.active_tab = index;
                 }
             }
 
-            if ui.button("+").clicked() {
+            // New t button
+            let plus_button = plus_button(ui);
+            if plus_button.clicked() {
                 self.tabs.push(types::Tab::new(config::DEFAULT_URL));
                 self.active_tab = self.tabs.len() - 1;
             }
@@ -51,4 +35,36 @@ impl<'a> TabBar<'a> {
 
         self.active_tab
     }
+}
+
+pub struct TabItem<'a> {
+    pub tab: &'a types::Tab,
+    pub is_active: bool,
+}
+
+impl<'a> TabItem<'a> {
+    pub fn new(tab: &'a types::Tab, is_active: bool) -> Self {
+        Self { tab, is_active }
+    }
+
+    pub fn show(&self, ui: &mut egui::Ui) -> egui::Response {
+        let url = self.tab.get_url();
+        let tab_name = if url.len() > 20 {
+            format!("{}...", &url[..20])
+        } else {
+            url.clone()
+        };
+
+        ui.add_sized(
+            [0.0, ui.available_size().y],
+            egui::SelectableLabel::new(self.is_active, tab_name),
+        )
+    }
+}
+
+fn plus_button(ui: &mut egui::Ui) -> egui::Response {
+    ui.add_sized(
+        [ui.available_size().y, ui.available_size().y],
+        egui::Button::new("+"),
+    )
 }
--- src/ui/config.rs
+++ src/ui/config.rs
@@ -1,5 +1,5 @@
 pub const TITLE: &str = "Oliver Explorer";
 pub const WIDGET_SIZE: f32 = 20.0;
-pub const TITLE_BAR_HEIGHT: f32 = 40.0;
+pub const TAB_BAR_HEIGHT: f32 = 40.0;
 pub const TEXT_SIZE: u16 = 8;
 pub const DEFAULT_URL: &str = "http://localhost:3333";
