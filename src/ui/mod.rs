0ffdeeb064ca0ec7d256b0c7b2f18abcb19a59cb
LoreviQ <lorevi@protonmail.com>
2025-03-11 19:26:49 +0000

    Removed default padding

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -6,10 +6,12 @@ use eframe::egui;
 
 impl eframe::App for state::OliverExplorer {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            for window in &mut self.windows {
-                window.update(ctx, ui);
-            }
-        });
+        egui::CentralPanel::default()
+            .frame(egui::Frame::new())
+            .show(ctx, |ui| {
+                for window in &mut self.windows {
+                    window.update(ctx, ui);
+                }
+            });
     }
 }
